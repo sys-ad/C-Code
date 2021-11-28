ce2fa0358d58ae735de3615b2bf5624641866903
sys-ad <95191519+sys-ad@users.noreply.github.com>
2021-11-28 17:57:08 -0500

    Hacker Rank: Algorithms - Problem Solving

---
+++ birthdayCakeCandles.c
@@ -0,0 +1,25 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+int i;
+int n;
+int max=0;
+scanf("%d",&n);
+int a[n];
+int count=0;
+
+for(i=1;i<=n;i++){
+scanf("%d",&a[i]);
+if(max<a[i])
+max=a[i];
+}
+for(i=1;i<=n;i++)
+if(a[i]==max)
+count++;
+
+printf("%d",count);
+return 0;
+}
+ 
